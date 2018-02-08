efb62b818b6b2ad605701cd7ca2a93b9012c2e87
Shuhei Nomura <info@danmaq.com>
2018-02-08 09:34:56 +0900

    Added ESLint.

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,3 @@
-'use strict';
-
 const gulp = require('gulp');
 const frontMatter = require('gulp-front-matter');
 const marked = require('gulp-marked');
@@ -11,17 +9,17 @@ const trace = require('./src/trace');
 const unEscapeLineBreak = require('./src/unEscapeLineBreak');
 
 gulp.task(
-    'translate',
-    () =>
-        gulp
-            .src('posts/ja/**/*.md')
-            //.src('posts/ja/blog/2017/04/25-problem-of-homebrew-cask-from-ansible.md')
-            .pipe(frontMatter({ property: 'frontmatter' }))
-            .pipe(marked({ xhtml: true, langPrefix: 'language-' }))
-            .pipe(escapeLineBreak())
-            .pipe(translate({ from: 'ja', to: 'en' }))
-            .pipe(unEscapeLineBreak())
-            .pipe(toMarkdown())
-            .pipe(trace())
-            .pipe(gulp.dest('posts/en'))
+  'translate',
+  () =>
+    gulp
+      .src('posts/ja/**/*.md')
+      // .src('posts/ja/blog/2017/04/25-problem-of-homebrew-cask-from-ansible.md')
+      .pipe(frontMatter({ property: 'frontmatter' }))
+      .pipe(marked({ xhtml: true, langPrefix: 'language-' }))
+      .pipe(escapeLineBreak())
+      .pipe(translate({ from: 'ja', to: 'en' }))
+      .pipe(unEscapeLineBreak())
+      .pipe(toMarkdown())
+      .pipe(trace())
+      .pipe(gulp.dest('posts/en')),
 );
